35e3a8808a4c9dc1ce86b869e9c1001685e59d5f
vkoorits <koorits.viktor@yandex.ru>
2019-08-18 17:41:37 +0300

    static json api for posts

--- main.go
+++ main.go
@@ -4,33 +4,41 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"encoding/json"
 )
 
 type Post struct {
-	Id uint64
-	Publisher string
-	PublishTime string
-	Text string 
+	Id uint64 `json:"post_id"`
+	Publisher string `json:"publisher"`
+	PublishTime string `json:"publish_time"`
+	Text string `json:"text"`
+}
+
+type RootPageHandler struct {
+	Tmpl *template.Template
+}
+
+func (h *RootPageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.Tmpl.Execute(w, nil)
+}
+
+func HandlePostsApi(w http.ResponseWriter, r *http.Request) {
+	posts := []Post{
+		Post{1, "vkoorits", "16 hours ago", "Пустите меня на танцпол пьяным подвигаться"},
+		Post{2, "vkoorits", "16 hours ago", "All those moments will be lost in time"},
+		Post{3, "anonim", "17 hours ago", "Остновите, Вите надо выйти"},
+	}
+	postsJson, _ := json.Marshal(posts)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(postsJson))
 }
 
 
+
 func main() {
-	tmpl := template.Must(template.ParseFiles("./template/index.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			posts := []Post{ // TODO change to API
-				Post{1, "vkoorits", "16 hours ago", "Пустите меня на танцпол пьяным подвигаться"},
-				Post{2, "vkoorits", "16 hours ago", "All those moments will be lost in time"},
-				Post{3, "anonim", "17 hours ago", "Остновите, Вите надо выйти"},
-			}
-
-			tmpl.Execute(w, struct {
-					Posts []Post
-				}{
-					posts,
-				})
-	})
-
+	rootHandler := &RootPageHandler{Tmpl: template.Must(template.ParseFiles("./template/index.html"))}
+	http.Handle("/", rootHandler)
+	http.HandleFunc("/api/posts", HandlePostsApi)
     http.Handle("/static/", http.StripPrefix(
         "/static/",
         http.FileServer(http.Dir("./static")),
